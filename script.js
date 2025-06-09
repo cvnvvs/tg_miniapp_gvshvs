6084bf87260a5d285045a04d761a67f0f1ffbcb9
null <cvnvvscvnt@gmail.com>
2025-06-09 07:52:38 +0000

    yyt

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // ВАЖНО: Перед запуском ngrok, вставьте сюда его АКТУАЛЬНЫЙ HTTPS URL
-const API_BASE_URL = 'https://https://bunny-brave-externally.ngrok-free.app'; 
+const API_BASE_URL = 'https://bunny-brave-externally.ngrok-free.app'; 
 
 const tg = window.Telegram.WebApp;
 
