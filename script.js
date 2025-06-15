1a7df95a680e6137a9aadd4484a3da2ecb756df9
null <cvnvvscvnt@gmail.com>
2025-06-15 19:34:33 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // ВАЖНО: Вставьте сюда ваш актуальный HTTPS URL от ngrok или вашего сервера
-const API_BASE_URL = 'https://upset-cases-visit.loca.lt'; 
+const API_BASE_URL = 'https://cvnvvs.ru'; 
 
 const tg = window.Telegram.WebApp;
 
