722963a56c7a743c205dfb31a1c00f1a057917ec
null <cvnvvscvnt@gmail.com>
2025-06-19 17:48:32 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'https://bunny-brave-externally.ngrok-free.app'; 
+const API_BASE_URL = 'https://cvnvvs.ru'; 
 
 const tg = window.Telegram.WebApp;
 
@@ -6,7 +6,7 @@ let appState = { userData: null, regData: {} };
 
 async function apiFetch(endpoint, options = {}) {
     const isPrivate = options.private !== false;
-    const headers = { 'Content-Type': 'application/json', 'ngrok-skip-browser-warning': 'true'};
+    const headers = { 'Content-Type': 'application/json' };
     if (isPrivate) {
         if (!tg.initData) throw new Error("Нет данных для авторизации.");
         headers['Authorization'] = `tma ${tg.initData}`;
